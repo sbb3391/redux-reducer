e2b27acf73245557a72d7c90347b6d28bf9ede3f
Sam Bequette <sbb3391@gmail.com>
2021-07-29 11:01:02 -0400

    commit tree d79174c7e62ffb18dbb3b55d13d88943f0edc717

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,8 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {numberOfPresents: 0}, action){
+  switch (action.type) {
+    case "Increase":
+      return Object.assign({}, state, {numberOfPresents: state.numberOfPresents + 1})
+    default:
+      return state; 
+  }
 }
